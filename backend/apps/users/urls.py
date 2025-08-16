01c37b5fcf67f26b7de95bf6577d3e88e21d3807
Sergo Azizbekyan <139613549+Pro100Sergosha@users.noreply.github.com>
2025-08-16 16:10:03 +0400

    Refactor(users): refactor user register

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,5 +3,4 @@ from .views import RegisterView, RegisterConfirmView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("register/confirm/", RegisterConfirmView.as_view(), name="register-confirm"),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -8,16 +8,8 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
-            response_data = serializer.save()
-            return Response(response_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RegisterConfirmView(APIView):
-    def post(self, request):
-        serializer = RegisterConfirmSerializer(data=request.data)
-        if serializer.is_valid():
-            response_data = serializer.save()
-            return Response(response_data, status=status.HTTP_201_CREATED)
-
+            serializer.save()
+            return Response(
+                {"message": "Registration successful"}, status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
