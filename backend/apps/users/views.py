9e84ce06dbe3c4b646329f22a45e45873d91e9c1
Sergo Azizbekyan <139613549+Pro100Sergosha@users.noreply.github.com>
2025-08-16 16:36:35 +0400

    Refactor(users): refactor users register functionality

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -8,18 +8,11 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
-            response_data = serializer.save()
-            return Response(response_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RegisterConfirmView(APIView):
-    def post(self, request):
-        serializer = RegisterConfirmSerializer(data=request.data)
-        if serializer.is_valid():
-            response_data = serializer.save()
-            return Response(response_data, status=status.HTTP_201_CREATED)
-
+            serializer.save()
+            return Response(
+                {"message": "User registered successfully."},
+                status=status.HTTP_201_CREATED,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
