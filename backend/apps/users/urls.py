d80aad33be2fb0038f2218397b616052fca32865
Sergo Azizbekyan <139613549+Pro100Sergosha@users.noreply.github.com>
2025-08-16 13:21:15 +0400

    Add login functionality (#5)
    * feature(users): add login functionality
    
    * test(users): add login tests

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, RegisterConfirmView
+from .views import *
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("register/confirm/", RegisterConfirmView.as_view(), name="register-confirm"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -21,3 +21,13 @@ class RegisterConfirmView(APIView):
             return Response(response_data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            response_data = serializer.save()
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
