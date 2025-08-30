6c6740661806030c3c45c0327ecd488d4ec0404d
Sergo Azizbekyan <139613549+Pro100Sergosha@users.noreply.github.com>
2025-08-30 15:25:32 +0400

    Refactor(frontend): refactor whole frontend project

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import "./HomePage.css"
+
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1 className='header-text'>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
